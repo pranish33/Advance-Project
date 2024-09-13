3cceea32447f1f48651c9f52ce6f05195a3b2849
Jitendra Shrestha <tjiten123@gmail.com>
2024-09-13 12:35:45 +1000

    Added Quality Assurance Report

--- Source Code/healthcarechatbotSystem/healthcarechatbotSystem/settings.py
+++ Source Code/healthcarechatbotSystem/healthcarechatbotSystem/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zeyroxcrestha@gmail.com'
-EMAIL_HOST_PASSWORD = 'jvcn fuwu ilks rapm'
+EMAIL_HOST_USER = 'healthcare@gmail.com'
+EMAIL_HOST_PASSWORD = 'app password here'
